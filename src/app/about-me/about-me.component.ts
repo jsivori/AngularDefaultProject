0fa38e2d2e70fcd5f2f1c7b14a972b334106f536
Jonatan Sivori <jonatansivori@MacBook-Pro-de-Jonatan.local>
2018-05-16 15:39:55 -0300

    node_modules

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -34,7 +34,8 @@ export class AboutMeComponent implements OnInit {
       "Particularmente, Angular en su ultima version al dia de la fecha me parecio un framework muy potente y sencillo para encarar aplicaciones mas que presentables a un usuario final, su sintaxis es facil de entender(en conceptos basicos) y hay documentacion que respalda mayormente los problemas que me fueron apareciendo durante la tarea de desarrollo.",
       "Para correr la app solamente se debe tener instalado el framework y basta con correr la directiva 'ng serve' dentro de la carpeta y ejecutar dentro de localhost: 4200 o el puerto especificado.",
       "Observaciones: El desarrollo se realizo en 3 dias, si bien he pedido una extension del plazo, como mencione, estoy recien llegado de un viaje y tuve que realizar tramites y una mudanza es por eso que pedi el plazo de extension ya que en dos dias llegaba pero a entregar algo no totalmente funcional, para el dia 2 a la noche tenia todo menos la funcionalidad de sorting de la tabla y no queria entregar con piezas faltantes de lo pedido.",
-      "Ademas agregue unas pantallas para hacer un poco mas realista el sitio y asi mismo esta mismo informacion fue puesto en el apartado 'Sobre' de la App."
+      "Ademas agregue unas pantallas para hacer un poco mas realista el sitio y asi mismo esta mismo informacion fue puesta en el apartado 'Sobre' de la App.",
+      "Pensaba en utilizar LocalStorage para la App pero con un arreglo como vi en los tutoriales me parecio suficiente y no queria excederme del plazo mas de lo actual."
     ];
   }
 
